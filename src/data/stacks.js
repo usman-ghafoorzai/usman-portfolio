18587ffc1add8ec718f57713b98241c187cf4332
Usman <usmang@ntnu.no>
2026-05-28 00:53:24 +0200

    add stack definitions file: define core stack categories for use across components

---
+++ src/data/stacks.js
@@ -0,0 +1,26 @@
+export const stacks = [
+    {
+        id: "frontend",
+        label: "Frontend",
+    },
+    {
+        id: "backend",
+        label: "Backend",
+    },
+    {
+        id: "databases",
+        label: "Databases",
+    },
+    {
+        id: "integration",
+        label: "Integration",
+    },
+    {
+        id: "systems-mobile",
+        label: "Systems & Mobile",
+    },
+    {
+        id: "workflow",
+        label: "Workflow",
+    },
+];
