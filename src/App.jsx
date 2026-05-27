3ed2c33a89f3ef27bbc13305198d78a067024390
Usman <usmang@ntnu.no>
2026-05-27 16:48:14 +0200

    add About section to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 import "./index.css";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
       <main className="page">
         <Navbar />
         <Hero />
+          <About />
 
         <section id="about" className="section">
           <div className="container">
